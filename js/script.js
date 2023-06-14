281785900fd07130f516efe21ae64943fe5ff6da
wesam-sakr <wesamsakr801@gmail.com>
2023-06-14 09:48:38 +0300

    intro

--- js/script.js
+++ js/script.js
@@ -1,108 +1,68 @@
 $(document).ready(function () {
   $('.jquery-background-video').bgVideo({fadeIn: 2000});
-
-var navbar = document.getElementsByTagName("nav");
-var sticky = navbar[0].offsetHeight ;
-
-
-// make nav bar static on scroll 
-window.addEventListener("scroll" , function(){
-    if (this.document.documentElement.scrollTop >= sticky) {
-        // $(navbar).css("position", "fixed");
-        $(navbar).css("background" , "#fff");
-    } else {
-        // $(navbar).removeClass("bg-main-color")
-        $(navbar).css("background" , "transparent");
-        // $(navbar).css("position", "sticky");
-    }
 })
+jQuery(document).ready(function() {
 
-  //loader
-  setTimeout(function () {
-    $(".loader").fadeOut(1000);
-  }, 2000);
-
-  $(".rev-client .owl-carousel").owlCarousel({
-    loop: true,
-    nav: true,
-    rtl: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      1000: {
-        items: 2,
-      },
-      1200: {
-        items: 3,
-      },
-    },
-  });
-  $('.partners .owl-carousel').owlCarousel({
-    center: true,
-    loop: true,
+  jQuery(".c-slider-init").slick({
+    dots: false,
     nav: false,
-    rtl: true,
-    margin: 50,
-    responsive: {
-      0: {
-        items: 3,
-      },
-      1000: {
-        items: 4,
-      },
-      1200: {
-        items: 5,
-      }
+    rtl:true,
+    arrows: false,
+    infinite: true,
+    // speed: 1200,
+    autoplaySpeed: 1000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    draggable: true,
+    pauseOnFocus: true,
+    pauseOnHover: true
+  });
+
+  jQuery(".slick-current").addClass("initialAnimation");
+
+  let transitionSetup = {
+    target: ".slick-list",
+    enterClass: "u-scale-out",
+    doTransition: function() {
+      var slideContainer = document.querySelector(this.target);
+      slideContainer.classList.add(this.enterClass);
+      jQuery(".slick-current").removeClass("animateIn");
     },
-  });
+    exitTransition: function() {
+      var slideContainer = document.querySelector(this.target);
+      setTimeout(() => {
+        slideContainer.classList.remove(this.enterClass);
+        jQuery(".slick-current").addClass("animateIn");
+      }, 2000);
+    }
+  };
 
-  // search sub menu
-  $(".top-nav-search .open-search").click(function (event) {
-    event.stopPropagation();
-    $(".top-nav-search form").toggleClass("show");
-    $(".top-nav-search input").focus();
-  });
-
-  $("body").click(function (e) {
-    let input = document.querySelector(".top-nav-search input");
-    if (e.target === input) {
-      $(".top-nav-search form").addClass("show");
-      $(".top-nav-search input").focus();
+  var i = 0;
+  // On before slide change
+  jQuery(".c-slider-init").on("beforeChange", function(
+                              event,
+                               slick,
+                               currentSlide,
+                               nextSlide
+                              ) {
+    if (i == 0) {
+      event.preventDefault();
+      transitionSetup.doTransition();
+      i++;
     } else {
-      $(".top-nav-search form").removeClass("show");
-    }
-  });
-
-  $(function () {
-    for (let i = 1; i <= 5; i++) {
-      $(".user-rate" + i).rateYo({
-        rating: i,
-        starWidth: "20px",
-        readOnly: true,
-        rtl: true,
-      });
+      i = 0;
+      transitionSetup.exitTransition();
     }
 
-    $("#your-rate").rateYo({
-      starWidth: "15px",
-      ratedFill: "#FFC107",
-      rating: 0,
-      fullStar: true,
-    });
+    jQuery(".c-slider-init").slick("slickNext");
+    jQuery(".slick-current").removeClass("initialAnimation");
   });
-  $(".blog .card").hover(function () {
-    $(this).find(".card-comm").stop().animate(
-      {
-        height: "toggle",
-        opacity: "toggle",
-      },
-      100
-    );
-  });
-
-  $('select').niceSelect();
-
+  new WOW().init();
 });
+
+
+
+// =========================
+
+
