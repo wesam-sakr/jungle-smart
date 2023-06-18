e1b6e933a6a28e43d8ec9f49dcfc34409e50d309
wesam-sakr <wesamsakr801@gmail.com>
2023-06-18 17:46:24 +0300

    services

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('select').niceSelect();
   $('.jquery-background-video').bgVideo({fadeIn: 2000});
-  
+
   // sev
   /*--------------------
   Vars
@@ -33,7 +33,6 @@ $(document).ready(function () {
     const zIndex = getZindex([...$items], active)[index]
     item.style.setProperty('--zIndex', zIndex)
     item.style.setProperty('--active', (index-active)/$items.length)
-    console.log(window.getComputedStyle(item).getPropertyValue('transform'))
   }
 
   /*--------------------
@@ -103,49 +102,50 @@ $(document).ready(function () {
   // --------
 
   // map
-	mapboxgl.accessToken = 'pk.eyJ1IjoiZWRrZiIsImEiOiJjamM2b2U4bDcwMm1tMndvNzI5dHQ1eTJtIn0.Ri6t-PZg3i5wivNlLehzVg';
-  // These options control the camera position after animation
-  const start = {
-      center: [-73.983970, 40.702710],
-      zoom: 2,
-      pitch: 0,
-      bearing: 0
-  };
-  const end = {
-      center: [-73.983970, 40.702710],
-      zoom: 12.5,
-      bearing: 0,
-      pitch: 0
-  };
-  const map = new mapboxgl.Map({
-      container: 'map',
-      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-      style: 'mapbox://styles/mapbox/light-v11',
-      ...start
-  });
-
-  map.on('style.load', () => {
-      // Custom atmosphere styling
-      map.setFog({
-          'color': 'rgb(220, 159, 159)', // Pink fog / lower atmosphere
-          'high-color': 'rgb(36, 92, 223)', // Blue sky / upper atmosphere
-          'horizon-blend': 0.4 // Exaggerate atmosphere (default is .1)
-      });
-
-      map.addSource('mapbox-dem', {
-          'type': 'raster-dem',
-          'url': 'mapbox://mapbox.terrain-rgb'
-      });
-
-      map.setTerrain({
-          'source': 'mapbox-dem',
-          'exaggeration': 1.5
-      });
-  });
-
-  let isAtStart = true;
-
-  document.getElementById('fly').addEventListener('click', () => {
+  if($('#location').length>0){
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZWRrZiIsImEiOiJjamM2b2U4bDcwMm1tMndvNzI5dHQ1eTJtIn0.Ri6t-PZg3i5wivNlLehzVg';
+    // These options control the camera position after animation
+    const start = {
+        center: [-73.983970, 40.702710],
+        zoom: 2,
+        pitch: 0,
+        bearing: 0
+    };
+    const end = {
+        center: [-73.983970, 40.702710],
+        zoom: 12.5,
+        bearing: 0,
+        pitch: 0
+    };
+    const map = new mapboxgl.Map({
+        container: 'map',
+        // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+        style: 'mapbox://styles/mapbox/light-v11',
+        ...start
+    });
+  
+    map.on('style.load', () => {
+        // Custom atmosphere styling
+        map.setFog({
+            'color': 'rgb(220, 159, 159)', // Pink fog / lower atmosphere
+            'high-color': 'rgb(36, 92, 223)', // Blue sky / upper atmosphere
+            'horizon-blend': 0.4 // Exaggerate atmosphere (default is .1)
+        });
+  
+        map.addSource('mapbox-dem', {
+            'type': 'raster-dem',
+            'url': 'mapbox://mapbox.terrain-rgb'
+        });
+  
+        map.setTerrain({
+            'source': 'mapbox-dem',
+            'exaggeration': 1.5
+        });
+    });
+  
+    let isAtStart = true;
+  
+    document.getElementById('fly').addEventListener('click', () => {
       // depending on whether we're currently at point a or b,
       // aim for point a or b
       const target = isAtStart ? end : start;
@@ -156,7 +156,10 @@ $(document).ready(function () {
           duration: 9000, // Animate over 12 seconds
           essential: false // This animation is considered essential with
           //respect to prefers-reduced-motion
-  });
+          
+      });
+    })
+  }
   // ------
   //loader
   setTimeout(function () {$(".loader").fadeOut(1000);})
@@ -196,7 +199,59 @@ $(document).ready(function () {
       }
     }
   });
+  $('.most-wanted-slider .owl-carousel').owlCarousel({
+    loop: true,
+    nav: true,
+    rtl: true,
+    margin: 10,
+    items: 4,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
   });
+
+  
+});
+  if($('.gallery-link').length>0){
+    (function() {
+      $('.gallery-link').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+          verticalFit: true,
+          titleSrc: function(item) {
+            return item.el.find('figcaption').text() || item.el.attr('title');
+          }
+        },
+        zoom: {
+          enabled: true
+        },
+        // duration: 300
+        gallery: {
+          enabled: true,
+          navigateByImgClick: false,
+          tCounter: ''
+        },
+        disableOn: function() {
+          return $(window).width() > 640;
+        }
+      });
+  
+    }).call(this);
+  }
   // =========================
   new WOW().init();
-});
+  // $('.multi-select').select2();
